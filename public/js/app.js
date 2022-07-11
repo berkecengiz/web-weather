4cdbdd0ac7afa9bd11291c5f8ce162378150c4a5
Berke Cengiz <Berke.Cengiz@ocean.ibm.com>
2022-07-11 10:27:45 +0200

    Inital commit

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-one')
+const messageTwo = document.querySelector('#message-two')
+
+// messageOne.textContent = 'Node.js weather web application.'
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = encodeURIComponent(search.value)
+
+    messageOne.textContent = 'Loading..'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+                console.log(data.error)
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('postman-request')
+
+const forecast = (latitude,longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=0bbe7e9f2f235537bd368b2d7c039a66&query=' + latitude + ',' + longitude 
+
+    request({url, json: true}, (error, {body}) =>{
+       if (error) {
+        callback('Unable to connect to weather services!', undefined)
+       } else if (body.error) {
+            callback('Unable to find location, try another search!', undefined)
+       } else {
+            callback(undefined, 'Today is '+ body.current.weather_descriptions[0] + '. It s currently '+ body.current.temperature + ' degress but it feels like ' + body.current.feelslike + ' degress.')
+       }
+    })
+}
+
+module.exports = forecast
+
